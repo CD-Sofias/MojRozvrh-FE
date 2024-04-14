a4293d3a65e83ef5c74e021cd6a1347a82f67af9
Artem Ponomarov <100004793+Aspergillusplay@users.noreply.github.com>
2024-04-14 13:18:12 +0200

    Fix path

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import {User} from "../types/user";
 import {AuthService} from "../auth/auth.service";
 
