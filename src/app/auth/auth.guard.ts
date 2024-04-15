1ae3dab8826ec77e2cf20c6d1b33b596b863ce1e
Artem Ponomarov <100004793+Aspergillusplay@users.noreply.github.com>
2024-04-15 17:41:44 +0200

    Moved logic from AuthGuard to AuthService

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,12 +1,12 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
+import {AuthService} from "./auth.service";
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const token = document.cookie.split(';').find(cookie => cookie.includes('accessToken'));
-  if (!token) {
-    router.navigate(['auth/login']);
-    return false;
+  const authService = inject(AuthService);
+  if (!authService.isAuthenticated()) {
+    return router.navigate(['auth/login']);
   }
   return true;
 };
