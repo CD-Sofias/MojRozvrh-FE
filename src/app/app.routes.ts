04acae68d46539e1f2799468a31e7ecca4209874
Artem Ponomarov <100004793+Aspergillusplay@users.noreply.github.com>
2024-05-24 14:23:59 +0200

    Refactor app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,34 +16,36 @@ import {myScheduleResolver} from "./my-schedule.resolver";
 import {ScheduleComponent} from "./dashboard/schedule/schedule.component";
 import {ScheduleCellComponent} from "./dashboard/admin/schedule-table-creator/schedule-cell/schedule-cell.component";
 
-export const routes: Routes = [
-  {path: '', redirectTo: 'schedule', pathMatch: 'full'},
-  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-  {
-    path: 'schedule', component: ScheduleComponent
-  },
-  {
-    path: 'my-schedule',
-    component: MyScheduleWrapperComponent,
-    children: [
-      {path: '', component: MyScheduleComponent},
-      {path: ':id', resolve: {schedule: myScheduleResolver}, component: MyScheduleDetailComponent},
-    ]
-  },
-  {
-    path: 'admin-panel', canActivate: [authGuard], component: ScheduleTableCreatorComponent, children: [
-      {path: '', redirectTo: 'teachers', pathMatch: 'full'},
-      {path: 'teachers', component: TeacherComponent},
-      {path: 'faculties', component: FacultyComponent},
-      {path: 'departments', component: DepartmentComponent},
-      {path: 'addresses', component: AddressesComponent},
-      {path: 'groups', component: GroupsComponent},
-      {path: 'classrooms', component: ClassroomsComponent},
-      {path: 'subjects', component: SubjectsComponent},
-      {path: 'schedule-cell', component: ScheduleCellComponent},
-    ]
-  },
-  {
-    path: 'dashboard', component: DashboardComponent, canActivate: [authGuard], children: []
-  },
-];
+export const routes: Routes = [{path: '', redirectTo: 'schedule', pathMatch: 'full'}, {
+  path: 'auth',
+  loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+}, {
+  path: 'schedule', component: ScheduleComponent
+}, {
+  path: 'my-schedule',
+  component: MyScheduleWrapperComponent,
+  children: [{path: '', component: MyScheduleComponent}, {
+    path: ':id',
+    resolve: {schedule: myScheduleResolver},
+    component: MyScheduleDetailComponent
+  },]
+}, {
+  path: 'admin-panel',
+  canActivate: [authGuard],
+  component: ScheduleTableCreatorComponent,
+  children: [{path: '', redirectTo: 'teachers', pathMatch: 'full'}, {
+    path: 'teachers',
+    component: TeacherComponent
+  }, {path: 'faculties', component: FacultyComponent}, {
+    path: 'departments',
+    component: DepartmentComponent
+  }, {path: 'addresses', component: AddressesComponent}, {
+    path: 'groups',
+    component: GroupsComponent
+  }, {path: 'classrooms', component: ClassroomsComponent}, {
+    path: 'subjects',
+    component: SubjectsComponent
+  }, {path: 'schedule-cell', component: ScheduleCellComponent},]
+}, {
+  path: 'dashboard', component: DashboardComponent, canActivate: [authGuard], children: []
+},];
